36bb33f6ddd880ce17edd86d39ed8379f64139bb
Крестников Константин Николаевич <KNKrestnikov@sberbank.ru>
2023-10-30 19:11:43 +0300

    Naming update

--- examples/giga_chain/server.py
+++ examples/giga_chain/server.py
@@ -10,6 +10,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models.gigachat import GigaChat
 from langchain.prompts import load_prompt
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.pydantic_v1 import BaseModel
 
 from langserve import add_routes
 
@@ -19,32 +20,25 @@ password = os.environ.get("GIGA_PASSWORD", None)
 logging.basicConfig(level=logging.INFO)
 
 
-def build_chain():
-    giga = GigaChat(
-        profanity=False,
-        verbose=True,
-        verify_ssl_certs=False,
-        user=user,
-        password=password,
-        base_url="https://wmapi-dev.saluteai-pd.sberdevices.ru/v1",
-        model="GigaChat-70b-4k-base:latest",
-        timeout=30,)
+giga = GigaChat(
+    profanity=False,
+    verbose=True,
+    timeout=30)
 
-    map_prompt = load_prompt("lc://prompts/summarize/map_reduce/map.yaml")
-    combine_prompt = load_prompt("lc://prompts/summarize/map_reduce/combine.yaml")
+map_prompt = load_prompt("lc://prompts/summarize/map_reduce/map.yaml")
+combine_prompt = load_prompt("lc://prompts/summarize/map_reduce/combine.yaml")
 
-    splitter = CharacterTextSplitter(chunk_size=12000, chunk_overlap=1000)
-    combine_chain = load_summarize_chain(
-        giga,
-        chain_type="map_reduce",
-        map_prompt=map_prompt,
-        combine_prompt=combine_prompt,
-    )
-    return AnalyzeDocumentChain(
-        combine_docs_chain=combine_chain, text_splitter=splitter
-    )
+splitter = CharacterTextSplitter(chunk_size=12000, chunk_overlap=1000)
+combine_chain = load_summarize_chain(
+    giga,
+    chain_type="map_reduce",
+    map_prompt=map_prompt,
+    combine_prompt=combine_prompt,
+)
 
-chain = build_chain()
+chain = AnalyzeDocumentChain(
+    combine_docs_chain=combine_chain, text_splitter=splitter
+)
 
 app = FastAPI(
     title="GigaChain Server",
@@ -62,7 +56,15 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
-add_routes(app, chain)
+# The input type is automatically inferred from the runnable
+# interface; however, if you want to override it, you can do so
+# by passing in the input_type argument to add_routes.
+class ChainInput(BaseModel):
+    """The input to the chain."""
+    token: str
+    input_document: str
+
+add_routes(app, chain, config_keys=["configurable"])
 
 if __name__ == "__main__":
     import uvicorn
