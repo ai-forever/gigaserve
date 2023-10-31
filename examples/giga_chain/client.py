590cf01b27d86a948d86cf71ed9c51ef7b4aa11a
Sergey Malyshev <in1t@ya.ru>
2023-11-01 00:14:33 +0300

    add forwarding headers to gigachat

--- examples/giga_chain/client.py
+++ examples/giga_chain/client.py
@@ -1,14 +1,24 @@
 """Пример вызова суммаризации статьи "Великий аттрактор" из Википедии"""
 import requests
 
-with open("examples/giga_chain/attractor.txt", "r", encoding="utf-8") as f:
-    TEXT = str(f.read())
+with open("attractor.txt", "r", encoding="utf-8") as f:
+    TEXT = f.read()
+
+with open("access_token.txt", "r", encoding="utf-8") as f:
+    ACCESS_TOKEN = f.read().strip()
 
 # Пример обращения с помощью requests
 response = requests.post(
     "http://localhost:8000/summarize/invoke",
     json={"input": {"input_document": TEXT}},
     timeout=600,
+    headers={
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        # for logging
+        "X-Client-ID": "8324244b-7133-4d30-a328-31d8466e5502",
+        "X-Session-ID": "8324244b-7133-4d30-a328-31d8466e5502",
+        "X-Request-ID": "8324244b-7133-4d30-a328-31d8466e5502",
+    }
 )
 resp = response.json()
 print(resp["output"]["output_text"])
--- examples/giga_chain/server.py
+++ examples/giga_chain/server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Example LangChain server exposes a chain composed of a prompt and an LLM."""
+import asyncio
 import logging
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,7 +17,7 @@ from langserve import add_routes
 logging.basicConfig(level=logging.INFO)
 
 
-giga = GigaChat(profanity=False, verbose=True, timeout=30)
+giga = GigaChat(profanity=False, verbose=True, timeout=30, verify_ssl_certs=False)
 
 map_prompt = load_prompt("lc://prompts/summarize/map_reduce/map.yaml")
 combine_prompt = load_prompt("lc://prompts/summarize/map_reduce/combine.yaml")
@@ -30,10 +32,22 @@ combine_chain = load_summarize_chain(
 
 chain = AnalyzeDocumentChain(combine_docs_chain=combine_chain, text_splitter=splitter)
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from contextvars_executor import ContextVarExecutor
+
+    # contextvars friendly ThreadPoolExecutor
+    loop = asyncio.get_running_loop()
+    loop.set_default_executor(ContextVarExecutor())
+    yield
+
+
 app = FastAPI(
     title="GigaChain Server",
     version="1.0",
     description="Spin up a simple api server using GigaChain's Runnable interfaces",
+    lifespan=lifespan,
 )
 
 # Set all CORS enabled origins
@@ -45,6 +59,20 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["*"],
 )
+
+
+@app.middleware("http")
+async def gigachat_headers(request, call_next):
+    from gigachat.context import authorization_cvar, client_id_cvar, session_id_cvar, request_id_cvar
+
+    # forwarding headers to gigachat
+    authorization_cvar.set(request.headers.get("Authorization"))
+    client_id_cvar.set(request.headers.get("X-Client-ID"))
+    session_id_cvar.set(request.headers.get("X-Session-ID"))
+    request_id_cvar.set(request.headers.get("X-Request-ID"))
+
+    return await call_next(request)
+
 
 add_routes(app, chain, path="/summarize")
 
